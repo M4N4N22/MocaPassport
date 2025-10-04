3ef300b534591e93cc28368c76fa059155e1634e
Manan Kapoor <108136108+M4N4N22@users.noreply.github.com>
2025-10-04 06:07:47 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ eslint: {
+    // disables ESLint warnings/errors during Vercel builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
